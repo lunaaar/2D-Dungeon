f1fe01d9e0831ce4d7f9a922b2d7e701be5d2159
Samuel Rubin <sjrube32@gmail.com>
2022-06-09 13:47:02 -0400

    Added movement
    added bare bones movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
-    
+    [Header("Movement")]
+    public float moveSpeed = 3f;
+    private float horizontalMovement;
+    private float verticalMovement;
+
+
+
+    private Rigidbody2D rb;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalMovement, verticalMovement).normalized * moveSpeed;
     }
 }
