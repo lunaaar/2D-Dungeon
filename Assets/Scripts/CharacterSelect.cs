4f46c8fc362f34d85f0b1cb587f47774fe0a20f6
Samuel Rubin <sjrube32@gmail.com>
2022-06-14 19:07:03 -0400

    Added menu stuff
    wrote character select script and has bare bones of functionality

--- Assets/Scripts/CharacterSelect.cs
+++ Assets/Scripts/CharacterSelect.cs
@@ -6,16 +6,45 @@ public class CharacterSelect : MonoBehaviour
 {
     public List<GameObject> playerPrefabs = new List<GameObject>(8);
 
+    public GameObject background;
+
+    private GameObject selectedCharacter;
+    private int activeIndex;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        selectedCharacter = playerPrefabs[0];
+        activeIndex = 0;
     }
 
+    
+    
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            activeIndex += 1;
+            if (activeIndex == 8)
+            {
+                activeIndex = 0;
+            }
+            
+            selectedCharacter = playerPrefabs[activeIndex];
+            background.transform.position = playerPrefabs[activeIndex].transform.position; 
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            activeIndex -= 1;
+            if (activeIndex == -1)
+            {
+                activeIndex = 7;
+            }
+
+            selectedCharacter = playerPrefabs[activeIndex];
+            background.transform.position = playerPrefabs[activeIndex].transform.position;
+        }
     }
 }
