9cc54f19f8111478e665f291be70163cdac34a2a
Samuel Rubin <sjrube32@gmail.com>
2022-06-15 09:27:29 -0400

    Improved Menu
    Added timer to the movement on character select screen, feels really good now. Most likely will switch to arrow.

--- Assets/Scripts/CharacterSelect.cs
+++ Assets/Scripts/CharacterSelect.cs
@@ -11,6 +11,9 @@ public class CharacterSelect : MonoBehaviour
     private GameObject selectedCharacter;
     private int activeIndex;
 
+    public float coolDownInSeconds;
+    private float currentTime;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,28 +26,64 @@ public class CharacterSelect : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Time.time > currentTime)
         {
-            activeIndex += 1;
-            if (activeIndex == 8)
+            if (Input.GetKey(KeyCode.RightArrow))
             {
-                activeIndex = 0;
-            }
-            
-            selectedCharacter = playerPrefabs[activeIndex];
-            background.transform.position = playerPrefabs[activeIndex].transform.position; 
-        }
+                activeIndex += 1;
+                activeIndex = activeIndex % 8;
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            activeIndex -= 1;
-            if (activeIndex == -1)
-            {
-                activeIndex = 7;
+                selectedCharacter = playerPrefabs[activeIndex];
+
+                // Tried MoveTowards, didn't give desired result. Speed ended up getting to a point of too high, and still didn't feel right because
+                // Position moved too fast for the movetowards. I need to figure out how to set a delay.
+                //background.transform.position = Vector3.MoveTowards(background.transform.position, playerPrefabs[activeIndex].transform.position, Time.deltaTime * speed);
+
+                background.transform.position = playerPrefabs[activeIndex].transform.position;
+                currentTime = Time.time + coolDownInSeconds;
             }
 
-            selectedCharacter = playerPrefabs[activeIndex];
-            background.transform.position = playerPrefabs[activeIndex].transform.position;
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                if (activeIndex - 1 < 0)
+                    activeIndex = 7;
+                else
+                    activeIndex -= 1;
+
+                selectedCharacter = playerPrefabs[activeIndex];
+                background.transform.position = playerPrefabs[activeIndex].transform.position;
+
+                currentTime = Time.time + coolDownInSeconds;
+            }
+
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                activeIndex += 4;
+                activeIndex = activeIndex % 8;
+
+                selectedCharacter = playerPrefabs[activeIndex];
+                background.transform.position = playerPrefabs[activeIndex].transform.position;
+
+                currentTime = Time.time + coolDownInSeconds;
+            }
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                if (activeIndex - 4 < 0)
+                    activeIndex += 4;
+                else
+                    activeIndex -= 4;
+
+                activeIndex = activeIndex % 8;
+
+                selectedCharacter = playerPrefabs[activeIndex];
+                background.transform.position = playerPrefabs[activeIndex].transform.position;
+
+                currentTime = Time.time + coolDownInSeconds;
+            }
         }
+
     }
 }
